e412555f207e0c3ce2f148702d3046866a8197a3
MJ <631359926@qq.com>
2017-04-17 10:06:58 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,41 @@
 <template>
-  <div id="app">
+  <div>
+    <v-header></v-header>
+    <div class="tab">
+      <div class="tab-item">
+        <router-link to="/">goods</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/star">star</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/seller">seller</router-link>
+      </div>
+    </div>
+    <!-- 路由出口 -->
+    <!-- 路由匹配到的组件将渲染在这里 -->
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import header from './components/header/header.vue'
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    'v-header': header
+  }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss" rel="stylesheet/scss">
+.tab {
+  display: flex;
+  height: 40px;
+  line-height: 40px;
+  .tab-item {
+    flex: 1;
+    text-align: center;
+  }
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Goods from '@/components/goods'
+import Star from '@/components/star'
+import Seller from '@/components/seller'
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Hello',
-      component: Hello
-    }
-  ]
+  routes: [{
+    path: '/',
+    name: 'Goods',
+    component: Goods
+  }, {
+    path: '/star',
+    name: 'Star',
+    component: Star
+  }, {
+    path: '/seller',
+    name: 'Seller',
+    component: Seller
+  }]
 })
