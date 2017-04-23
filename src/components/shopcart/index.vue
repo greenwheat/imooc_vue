3622b44e6a91347dd8548c835ad09e8517c0b76b
MJ <631359926@qq.com>
2017-04-23 17:50:17 +0800

    购物车列表完成

--- src/components/shopcart/index.vue
+++ src/components/shopcart/index.vue
@@ -1,149 +1,221 @@
 <template>
-<div class="shopcart">
-  <div class="content">
-    <div class="content-left">
-      <div class="logo-wrapper">
-        <div class="logo" :class="{'highlight':totalCount>0}">
-          <i class="icon-shopping_cart" :class="{'highlight':totalCount>0}"></i>
-        </div>
-        <div class="num" v-show="totalCount>0">{{totalCount}}</div>
-      </div>
-      <div class="price" :class="{'highlight':totalCount>0}">￥{{totalPrice}}</div>
-      <div class="desc">另需配送费￥{{deliveryPrice}}元</div>
-    </div>
-    <div class="content-right">
-      <div class="pay" :class="payClass">
-        {{payDesc}}
-      </div>
-    </div>
-  </div>
-  <transition-group tag="div" class="ball-container" name="drop" @before-enter="beforeEnter" @enter="enter" @after-enter="afterEnter">
-    <div v-for="(ball,$index) in balls" :key="$index" v-show="ball.show" class="ball">
-      <div class="inner inner-hook">
-      </div>
-    </div>
-  </transition-group>
+<div class="shopcart-box">
+	<div class="shopcart">
+		<div class="content" @click="toggleList">
+			<div class="content-left">
+				<div class="logo-wrapper">
+					<div class="logo" :class="{'highlight':totalCount>0}">
+						<i class="icon-shopping_cart" :class="{'highlight':totalCount>0}"></i>
+					</div>
+					<div class="num" v-show="totalCount>0">{{totalCount}}</div>
+				</div>
+				<div class="price" :class="{'highlight':totalCount>0}">￥{{totalPrice}}</div>
+				<div class="desc">另需配送费￥{{deliveryPrice}}元</div>
+			</div>
+			<div class="content-right" @click.stop.prevent="pay">
+				<div class="pay" :class="payClass">
+					{{payDesc}}
+				</div>
+			</div>
+		</div>
+		<transition-group tag="div" class="ball-container" name="drop" @before-enter="beforeEnter" @enter="enter" @after-enter="afterEnter">
+			<div v-for="(ball,$index) in balls" :key="$index" v-show="ball.show" class="ball">
+				<div class="inner inner-hook">
+				</div>
+			</div>
+		</transition-group>
+		<transition name="fold">
+			<div class="shopcart-list" v-show="listShow">
+				<div class="list-header">
+					<div class="title">购物车</div>
+					<div class="empty" @click="empty">清空</div>
+				</div>
+				<div class="list-content" ref="listContent">
+					<ul>
+						<li class="food border-1px border-bottom" v-for="food in selectFoods">
+							<span class="name">{{food.name}}</span>
+							<div class="price">
+								<span>￥{{food.price*food.count}}</span>
+							</div>
+							<div class="cartcontrol-warpper">
+								<cartcontrol :food="food"></cartcontrol>
+							</div>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</transition>
+	</div>
+	<transition name="fade">
+		<div class="list-mask" @click="hideList" v-show="listShow"></div>
+	</transition>
 </div>
 </template>
 
 <script type="text/ecmascript-6">
+import BScroll from 'better-scroll'
+import cartcontrol from '@/components/cartcontrol'
+
 export default {
-  props: {
-    selectFoods: {
-      type: Array,
-      default () {
-        return []
-      }
-    },
-    deliveryPrice: {
-      type: Number,
-      default: 0
-    },
-    minPrice: {
-      type: Number,
-      default: 0
-    }
-  },
-  data() {
-    return {
-      balls: [{
-        show: false
-      }, {
-        show: false
-      }, {
-        show: false
-      }, {
-        show: false
-      }, {
-        show: false
-      }],
-      dropBalls: []
-    }
-  },
-  computed: {
-    totalPrice() {
-      let total = 0
-      this.selectFoods.forEach((food) => {
-        total += food.price * food.count
-      })
-      return !isNaN(total) ? total : 0
-    },
-    totalCount() {
-      let total = 0
-      this.selectFoods.forEach((food) => {
-        total += food.count
-      })
-      return !isNaN(total) ? total : 0
-    },
-    payDesc() {
-      if (this.totalPrice === 0) {
-        return `￥${this.minPrice}元起送`
-      } else if (this.totalPrice < this.minPrice) {
-        let diff = this.minPrice - this.totalPrice
-        return `还差￥${diff}元起送`
-      } else {
-        return '去结算'
-      }
-    },
-    payClass() {
-      if (this.totalPrice < this.minPrice) {
-        return 'not-enough'
-      } else {
-        return 'enough'
-      }
-    }
-  },
-  methods: {
-    drop(el) {
-      // console.log(el)
-      for (let i = 0; i < this.balls.length; i++) {
-        let ball = this.balls[i]
-        if (!ball.show) {
-          ball.show = true
-          ball.el = el
-          this.dropBalls.push(ball)
-          return
-        }
-      }
-    },
-    beforeEnter(el) {
-      let count = this.balls.length
-      // console.log('count', count)
-      while (count--) {
-        let ball = this.balls[count]
-        if (ball.show) {
-          let rect = ball.el.getBoundingClientRect()
-          let x = rect.left - 32
-          let y = -(window.innerHeight - rect.top - 22)
-          el.style.display = ''
-          el.style.webkitTransform = `translate3d(0,${y}px,0)`
-          el.style.transform = `translate3d(0,${y}px,0)`
-          let inner = el.getElementsByClassName('inner-hook')[0]
-          inner.style.webkitTransform = `translate3d(${x}px,0,0)`
-          inner.style.transform = `translate3d(${x}px,0,0)`
-        }
-      }
-    },
-    enter(el) {
-      /* eslint-disable no-unused-vars */
-      let rf = el.offsetHeight
-      console.log(rf)
-      this.$nextTick(() => {
-        el.style.webkitTransform = 'translate3d(0,0,0)'
-        el.style.transform = 'translate3d(0,0,0)'
-        let inner = el.getElementsByClassName('inner-hook')[0]
-        inner.style.webkitTransform = 'translate3d(0,0,0)'
-        inner.style.transform = 'translate3d(0,0,0)'
-      })
-    },
-    afterEnter(el) {
-      let ball = this.dropBalls.shift()
-      if (ball) {
-        ball.show = false
-        el.style.display = 'none'
-      }
-    }
-  }
+	props: {
+		selectFoods: {
+			type: Array,
+			default() {
+				return []
+			}
+		},
+		deliveryPrice: {
+			type: Number,
+			default: 0
+		},
+		minPrice: {
+			type: Number,
+			default: 0
+		}
+	},
+	data() {
+		return {
+			balls: [{
+				show: false
+			}, {
+				show: false
+			}, {
+				show: false
+			}, {
+				show: false
+			}, {
+				show: false
+			}],
+			dropBalls: [],
+			fold: false
+		}
+	},
+	components: {
+		cartcontrol
+	},
+	computed: {
+		totalPrice() {
+			let total = 0
+			this.selectFoods.forEach((food) => {
+				total += food.price * food.count
+			})
+			return !isNaN(total) ? total : 0
+		},
+		totalCount() {
+			let total = 0
+			this.selectFoods.forEach((food) => {
+				total += food.count
+			})
+			return !isNaN(total) ? total : 0
+		},
+		payDesc() {
+			if(this.totalPrice === 0) {
+				return `￥${this.minPrice}元起送`
+			} else if(this.totalPrice < this.minPrice) {
+				let diff = this.minPrice - this.totalPrice
+				return `还差￥${diff}元起送`
+			} else {
+				return '去结算'
+			}
+		},
+		payClass() {
+			if(this.totalPrice < this.minPrice) {
+				return 'not-enough'
+			} else {
+				return 'enough'
+			}
+		},
+		listShow() {
+			if(!this.totalCount) {
+				this.fold = true
+				return false
+			}
+			let show = !this.fold
+			if(show) {
+				this.$nextTick(() => {
+					if(!this.scroll) {
+						this.scroll = new BScroll(this.$refs.listContent, {
+							click: true
+						})
+					} else {
+						this.scroll.refresh()
+					}
+				})
+			}
+			return show
+		}
+	},
+	methods: {
+		drop(el) {
+			// console.log(el)
+			for(let i = 0; i < this.balls.length; i++) {
+				let ball = this.balls[i]
+				if(!ball.show) {
+					ball.show = true
+					ball.el = el
+					this.dropBalls.push(ball)
+					return
+				}
+			}
+		},
+		beforeEnter(el) {
+			let count = this.balls.length
+			// console.log('count', count)
+			while(count--) {
+				let ball = this.balls[count]
+				if(ball.show) {
+					let rect = ball.el.getBoundingClientRect()
+					let x = rect.left - 32
+					let y = -(window.innerHeight - rect.top - 22)
+					el.style.display = ''
+					el.style.webkitTransform = `translate3d(0,${y}px,0)`
+					el.style.transform = `translate3d(0,${y}px,0)`
+					let inner = el.getElementsByClassName('inner-hook')[0]
+					inner.style.webkitTransform = `translate3d(${x}px,0,0)`
+					inner.style.transform = `translate3d(${x}px,0,0)`
+				}
+			}
+		},
+		enter(el) {
+			/* eslint-disable no-unused-vars */
+			let rf = el.offsetHeight
+			console.log(rf)
+			this.$nextTick(() => {
+				el.style.webkitTransform = 'translate3d(0,0,0)'
+				el.style.transform = 'translate3d(0,0,0)'
+				let inner = el.getElementsByClassName('inner-hook')[0]
+				inner.style.webkitTransform = 'translate3d(0,0,0)'
+				inner.style.transform = 'translate3d(0,0,0)'
+			})
+		},
+		afterEnter(el) {
+			let ball = this.dropBalls.shift()
+			if(ball) {
+				ball.show = false
+				el.style.display = 'none'
+			}
+		},
+		toggleList() {
+			if(!this.totalCount) {
+				return
+			}
+			this.fold = !this.fold
+		},
+		empty() {
+			this.selectFoods.forEach((food) => {
+				food.count = 0
+			})
+		},
+		hideList() {
+			this.fold = true
+		},
+		pay() {
+			if(this.totalPrice < this.minPrice) {
+				return
+			}
+			window.alert(`支付${this.totalPrice}元`)
+		}
+	}
 }
 </script>
 
@@ -272,6 +344,93 @@ export default {
                 }
             }
         }
+    }
+    .shopcart-list {
+        position: absolute;
+        left: 0;
+        top: 0;
+        z-index: -1;
+        width: 100%;
+        transform: translate3d(0, -100%, 0);
+
+        &.fold-enter-active,
+        &.fold-leave-active {
+            transition: all 0.5s;
+            /* transform: translate3d(0,-100%,0); */
+        }
+        &.fold-enter,
+        &.fold-leave-active {
+            transform: translate3d(0,0,0);
+        }
+        .list-header {
+            height: 40px;
+            line-height: 40px;
+            padding: 0 18px;
+            background: #f3f5f7;
+            border-bottom: 1px solid rgba(7,17,27,0.1);
+            .title {
+                float: left;
+                font-size: 14px;
+                color: #07111b;
+            }
+            .empty {
+                float: right;
+                font-size: 12px;
+                color: #00a0dc;
+            }
+        }
+        .list-content {
+            padding: 0 18px;
+            max-height: 217px;
+            overflow: hidden;
+            background: #fff;
+            .food {
+                position: relative;
+                padding: 12px 0;
+                box-sizing: border-box;
+                position: relative;
+                .name {
+                    line-height: 24px;
+                    font-size: 14px;
+                    color: #07111b;
+                }
+                .price {
+                    position: absolute;
+                    right: 90px;
+                    bottom: 12px;
+                    line-height: 24px;
+                    font-size: 14px;
+                    font-weight: 700;
+                    color: #f01414;
+                }
+                .cartcontrol-warpper {
+                    position: absolute;
+                    right: 0;
+                    bottom: 6px;
+                }
+            }
+        }
+    }
+}
+
+.list-mask {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 40;
+    -webkit-backdrop-filter: blur(10px);
+    backdrop-filter: blur(10px);
+    opacity: 1;
+    background: rgba(7,17,27,0.6);
+    &.fade-enter-active,
+    &.fade-leave-active {
+        transition: opacity 0.5s;
+    }
+    &.fade-enter,
+    &.fade-leave-active {
+        opacity: 0;
     }
 }
 </style>
